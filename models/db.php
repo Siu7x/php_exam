0e1169191c78fb542abce882b087c0838df87a18
Kenan <munsch.kenan@ynov.com>
2026-03-05 19:15:43 +0100

    resolutino bug

--- models/db.php
+++ models/db.php
@@ -1,15 +1,1 @@
 <?php
-// models/db.php
-
-$host = "localhost";
-$user = "root";
-$pass = "root"; // Si erreur, remplace par "" 
-$dbname = "php_exam"; // Nom de ta base de données [cite: 159]
-
-$mysqli = new mysqli($host, $user, $pass, $dbname);
-
-// Optionnel mais recommandé : vérifier la connexion
-if ($mysqli->connect_error) {
-    die("Échec de la connexion : " . $mysqli->connect_error);
-}
-?>
